baa3c7e5a9c79dc9b233b6fdbb265a42551efe2c
nathan <145555479+ntyx@users.noreply.github.com>
2026-06-22 00:25:08 +0800

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # for row in board:
+        #     arr = [0] * 9
+        #     for i in range(len(row)):
+        #         if row[i] != '.':
+        #             arr[int(row[i]) - 1] += 1
+        #             if arr[int(row[i]) - 1] > 1:
+        #                 return False
+        # for i in range(len(board)):
+        #     arr = [0] * 9
+        #     for j in range(len(board)):
+        #         if board[j][i] != '.':
+        #             arr[int(board[j][i]) - 1] += 1
+        #             if arr[int(board[j][i]) - 1] > 1:
+        #                 return False
+        # x = 0
+        # for topoffset in range(0, 9, 3):
+        #     for leftoffset in range(0, 9, 3):
+        #         arr = [0] * 9
+        #         for i in range(0, 3, 1):
+        #             for j in range(0,3,1):
+        #                 if board[i + topoffset][j + leftoffset] != '.':
+        #                     arr[int(board[i + topoffset][j + leftoffset]) - 1] += 1
+        #                     if arr[int(board[i + topoffset][j + leftoffset]) - 1] > 1:
+        #                         return False
+        # return True
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        square = defaultdict(set)
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] != '.':
+                    if (board[i][j] in rows[i] or board[i][j] in cols[j] or
+                        board[i][j] in square[(i//3,j//3)]):
+                        return False
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    square[i//3,j//3].add(board[i][j])
+        return True
+            
